adb11e7d832b764be8a006e145150a1c2f168e41
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-30 01:51:35 +0100

    Add formula for version 1.0.0.

---
+++ Formula/timer-for-python.rb
@@ -0,0 +1,19 @@
+class ColoristForPython < Formula
+  include Language::Python::Virtualenv
+
+  desc "Make it easy and fast to print terminal messages in colors."
+  homepage "https://github.com/jakob-bagterp/colorist-for-python"
+  url "https://github.com/jakob-bagterp/colorist-for-python/releases/download/v1.0.0/colorist-1.0.0.tar.gz"
+  sha256 "84ce6a01609ba4196fe254bdd9eaf69c89cfffa8c64808060e29bdb8edd82485"
+  license "MIT"
+
+  depends_on "python@3.9"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "true"
+  end
+end
