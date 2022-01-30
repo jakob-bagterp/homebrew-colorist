bafbbc7fc9374cb871e9d697ceac5f29e665dead
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-30 02:14:59 +0100

    Fix audit issue "Description shouldn't end with a full stop".

--- Formula/colorist.rb
+++ Formula/colorist.rb
@@ -1,7 +1,7 @@
 class Colorist < Formula
   include Language::Python::Virtualenv
 
-  desc "Make it easy and fast to print terminal messages in colors."
+  desc "Make it easy and fast to print terminal messages in colors"
   homepage "https://github.com/jakob-bagterp/colorist-for-python"
   url "https://github.com/jakob-bagterp/colorist-for-python/releases/download/v1.0.0/colorist-1.0.0.tar.gz"
   sha256 "84ce6a01609ba4196fe254bdd9eaf69c89cfffa8c64808060e29bdb8edd82485"
