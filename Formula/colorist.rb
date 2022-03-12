aa6a5254c5caebeb394ed817e69ad69ed7d92634
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-12 13:16:22 +0100

    Update formula to release version 1.2.1.

--- Formula/colorist.rb
+++ Formula/colorist.rb
@@ -3,8 +3,8 @@ class Colorist < Formula
 
   desc "Make it easy and fast to print terminal messages in colors"
   homepage "https://github.com/jakob-bagterp/colorist-for-python"
-  url "https://github.com/jakob-bagterp/colorist-for-python/releases/download/v1.2.0/colorist-1.2.0.tar.gz"
-  sha256 "7b72206d15b377ae96f08f407cdfbe34e080fff7094caa143f256061341edb80"
+  url "https://github.com/jakob-bagterp/colorist-for-python/releases/download/v1.2.1/colorist-1.2.1.tar.gz"
+  sha256 "d769a074136b3937ff551e38e92483a90d5fbe80e0ce0b192ec5e7a01b1f97db"
   license "MIT"
 
   depends_on "python@3.10"
